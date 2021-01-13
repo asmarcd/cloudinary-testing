fa75b8b8da138a89454f535909c13a7e0e43e95b
Chris Asmar <asmarcd@gmail.com>
2021-01-13 10:05:05 -0800

    create and destroy routes added

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,9 @@
 class ImagesController < ApplicationController
+    def create
+        Cloudinary::Uploader.upload()
+    end
+
+    def destroy
+        Cloudinary::Uploader.destroy(id)
+    end
 end
