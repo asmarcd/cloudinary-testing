a5d16240aaa6f44e13dbdee07d55e2e3c112148c
Chris Asmar <asmarcd@gmail.com>
2021-01-13 10:48:24 -0800

    react started

--- app/javascript/components/Upload.js
+++ app/javascript/components/Upload.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Upload = () => {
-    return (
-        <div>Upload button goes here</div>
-    )
+
+    const [image, setImage] = useState({});
+
+    onChange = (e) => {
+        e.persist()
+        this.setState(() => {
+            return {
+                [e.target.name]: e.target.files[0]
+            }
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        const form = new FormData()
+        form.append("image", this.state.image)
+        fetch(`http://localhost:3000/images`, {
+            method: "POST",
+            body: form
+        })
+    }
+
+        return (
+            <div className="form">
+                <h1>New Upload</h1>
+                <form onSubmit={this.onSubmit}>
+                    <label>Image Upload</label>
+                    <input type="file" name="image" onChange={this.onChange}/>
+                    <br/>
+                    <input type="submit"/>
+                </form>
+            </div>
+        )    
 };
 
 export default Upload;
