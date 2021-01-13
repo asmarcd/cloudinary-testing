5a38c621f07c384734014bebd6b39e97386e0f62
Chris Asmar <asmarcd@gmail.com>
2021-01-13 09:35:34 -0800

    database, controllers, and cloudinary connection

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|    
+    config.cloud_name = "asmarphotocloud"   
+    config.api_key = "696138154388777"   
+    config.api_secret = "7kjEUrRjvy10aTdGz0arrbMyVbI"   
+    config.secure = true    
+    config.cdn_subdomain = true  
+ end
---
+++ db/migrate/20210113172723_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :images do |t|
+      t.string :title
+      t.string :caption
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
